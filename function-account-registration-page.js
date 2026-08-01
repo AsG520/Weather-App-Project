a7528e50b13db933773f51d84a34c491722c9812
AsG520 <asgirn520@gmail.com>
2026-08-01 16:57:16 -0400

    Completed designing the account registration page and assigned index.html to login page, meaning this is the root file for the app.

---
+++ function-account-registration-page.js
@@ -0,0 +1,7 @@
+let loginWord = document.getElementById("login-word");
+
+function callLoginPage() {
+    window.location.href = "index.html";
+}
+
+loginWord.addEventListener("click", callLoginPage);
---
+++ function.js
@@ -0,0 +1,7 @@
+let registerWord = document.getElementById("register-word");
+
+function callAccountRegistrationPage() {
+    window.location.href = "index-account-registration-page.html";
+}
+
+registerWord.addEventListener("click", callAccountRegistrationPage);
